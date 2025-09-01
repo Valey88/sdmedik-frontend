25cf71b4eb02c248e0a227c55b2efe78f92c177e
Valey88 <valeywokr200501@gmail.com>
2025-09-01 10:29:54 +0400

    add basket in header

--- src/global/header.jsx
+++ src/global/header.jsx
@@ -8,6 +8,7 @@ import {
   Button,
   Paper,
   Drawer,
+  Badge,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import React, { useState, useEffect } from "react";
@@ -21,6 +22,7 @@ import Search from "./components/Search";
 import BurgerMenu from "./components/BurgerMenu";
 import Navigation from "./components/Navigation";
 import { Link, useNavigate } from "react-router-dom";
+import useBascketStore from "../store/bascketStore";
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   display: "flex",
@@ -38,6 +40,11 @@ export default function Header() {
     useUserStore();
   const { getUserInfo, user, logout } = useUserStore();
   const navigate = useNavigate();
+  const { fetchUserBasket, basket } = useBascketStore();
+
+  useEffect(() => {
+    fetchUserBasket();
+  }, []);
 
   useEffect(() => {
     checkAuthStatus();
@@ -128,6 +135,7 @@ export default function Header() {
         >
           <Logo />
           <Search />
+
           <IconButton
             edge="start"
             color="inherit"
@@ -167,25 +175,50 @@ export default function Header() {
           }}
         >
           <Logo />
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            onClick={toggleDrawer(true)}
-            sx={{
-              display: {
-                xs: "block",
-                sm: "block",
-                md: "block",
-                lg: "none",
-                color: "#26BDB8",
-              },
-            }}
-          >
-            <MenuIcon fontSize="large" />
-          </IconButton>
+          <Box sx={{ display: "flex" }}>
+            <Link to="/basket">
+              <IconButton onClick={toggleDrawer(false)}>
+                <Badge
+                  badgeContent={basket?.data?.quantity}
+                  color="primary"
+                  overlap="circular"
+                  sx={{
+                    "& .MuiBadge-badge": {
+                      backgroundColor: "#26BDB8",
+                      color: "white",
+                    },
+                  }}
+                >
+                  <img
+                    style={{ width: 45, height: 45 }}
+                    src="/basket_header.png"
+                    alt="basket"
+                  />
+                </Badge>
+              </IconButton>
+            </Link>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={toggleDrawer(true)}
+              sx={{
+                display: {
+                  xs: "block",
+                  sm: "block",
+                  md: "block",
+                  lg: "none",
+                  color: "#26BDB8",
+                },
+              }}
+            >
+              <MenuIcon fontSize="large" />
+            </IconButton>
+          </Box>
         </Box>
+
         <Search />
+
         <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
           <BurgerMenu toggleDrawer={toggleDrawer} />
         </Drawer>
